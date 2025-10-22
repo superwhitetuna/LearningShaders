86abdd9956d9fe35fbf66bc7a168e683082ff5ad
superwhitetuna <csulei@outlook.com>
2025-10-21 22:05:13 -0700

    the learning is real.

--- myshader.glsl
+++ myshader.glsl
@@ -4,7 +4,7 @@ float sdCircle(vec2 p, float r)
 }
 float random(vec2 st)
 {
-    return fract(sin(dot(st, vec2(10, 40)))* 10000.0);
+    return (fract(sin(dot(st, vec2(12.9898, 78.233))) * 43758.5453123));
 }
 float noise(vec2 st)
 {
@@ -23,13 +23,44 @@ vec3 palette(float t)
     vec3 b = vec3(0.5, 0.5, 0.5);
     vec3 c = vec3(1.0, 1.0, 1.0);
     vec3 d = vec3(0.259, 0.416, 0.557);
-    return a + b * cos(6.28318 * (c * t + d));
+    return c * cos(6.28318 * (a + b + c * t + d));
 }
+
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
     vec2 uv = fragCoord / iResolution.xy;
     uv = uv * 2.0 - 1.0;
-    float d = sdCircle(uv - vec2(sin(iTime * 2.0), sin(iTime * 3.0)), sin(abs(1.0 + sin(iTime))));
+    
+    vec2 mainCenter = vec2(sin(iTime * 2.0), sin(iTime * 3.0));
+    float mainRadius = sin(abs(1.0 + sin(iTime)));
+    float mainCircle = sdCircle(uv - mainCenter, mainRadius);
+
+    vec2 offset = vec2( -1.0, 1.5);
+    vec2 smallCenter = mainCenter + offset;
+    float smallRadius = mainRadius * 0.5;
+    float d2 = sdCircle(uv - smallCenter, smallRadius);
+
+    vec2 offset3 = vec2( 1.0, 1.5);
+    vec2 small3Center = mainCenter + offset3;
+    float d3 = sdCircle(uv - small3Center, smallRadius);
+
+    vec2 offset4 = vec2(-1.0, -1.5);
+    vec2 small4Center = mainCenter + offset4;
+    float d4 = sdCircle(uv - small4Center, smallRadius);
+
+    vec2 offset5 =  vec2(1.0, -1.5);
+    vec2 small5Center = mainCenter + offset5;
+    float d5 = sdCircle(uv - small5Center, smallRadius);
+
+    vec2 small6Center = vec2(-sin(iTime * 2.0), -sin(iTime * 3.0));
+    // float small6Radius = mainRadius * 0.3;
+    float d6 = sdCircle(uv - small6Center, 0.3);
+
+
+    float d = min(mainCircle, min(d2, min(d3, min(d4, d5))));
+    
+    float s = min(d6, d6);
+       
     vec3 col;
     if (d < 0.0) {
         float t = iTime * 0.1;
@@ -42,8 +73,16 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
         col = vec3(0.7 - 0.3 + uv.y * uv.x * d * sin(cos(iTime)) * 2.0);
     }
 
+    if (s < 0.0) {
+        col = vec3(abs(sin(col))); 
+    }
+    else if (s > 0.0 && s < 0.1) {
+        vec3 outcol = vec3(0.7 - 0.3 + uv.y * uv.x * d * sin(cos(iTime)) * 2.0, 0.2, 0.2);
+        col = vec3(abs(sin(outcol)));
+    }
+
     vec3 outcol = vec3(0.7 - 0.3 + uv.y * uv.x * d * sin(cos(iTime)) * 2.0);
-    float edgeWidth = 5.0;
+    float edgeWidth = 10.0;
     float t = smoothstep(-edgeWidth, edgeWidth, d);
     col = mix(col, outcol, t);
 
