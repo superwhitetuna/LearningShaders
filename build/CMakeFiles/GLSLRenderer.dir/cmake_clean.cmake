49cfeb4b9b1391e02d213bfdf1d55c060e3f3f13
s u l i <86400598+superwhitetuna@users.noreply.github.com>
2025-10-26 23:28:06 -0700

    macOS homebrew GLSL renderer

---
+++ build/CMakeFiles/GLSLRenderer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GLSLRenderer.dir/main.cpp.o"
+  "CMakeFiles/GLSLRenderer.dir/main.cpp.o.d"
+  "GLSLRenderer"
+  "GLSLRenderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GLSLRenderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
